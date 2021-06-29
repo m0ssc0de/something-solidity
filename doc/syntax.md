e63aa626401cdf2027251d7ac3fbd19c0c7a29f0
m0ssc0de <hi.paul.q@gmail.com>
2021-06-29 22:49:59 +0800

    update

---
+++ doc/syntax.md
@@ -0,0 +1,25 @@
+# syntax
+
+https://docs.soliditylang.org/en/v0.6.7/structure-of-a-contract.html
+
+above is breaf description.
+
+https://docs.soliditylang.org/en/v0.6.8/cheatsheet.html
+
+https://www.alibabacloud.com/help/doc-detail/109251.htm
+
+ali BaaS is different with solidity.
+
+base syntax.
+
+then read from first page.
+
+Remix don't need a server side. means other ide needd.
+
+Dapp a toolchain.
+
+public on function, will make function accessiable from outside and interside.
+
+public on variable, will make variable not only accesiable and automatically get a getter.
+
+mapping on variable, not only declare a map, also create a getter func.
--- src/test.sol
+++ src/test.sol
@@ -6,4 +6,31 @@ contract HelloWorldContract {
     function helloWorld() external pure returns(string memory) {
         return "hello world";
     }
+    function haha() external pure returns(int) {
+        return 123;
+    }
+    function testkw0() public pure returns(int) {
+        return 222;
+    }
+
+        enum ActionChoices { GoLeft, GoRight, GoStraight, SitStill }
+    ActionChoices choice;
+    ActionChoices constant defaultChoice = ActionChoices.GoStraight;
+
+    function setGoStraight() public {
+        choice = ActionChoices.GoStraight;
+    }
+
+    // Since enum types are not part of the ABI, the signature of "getChoice"
+    // will automatically be changed to "getChoice() returns (uint8)"
+    // for all matters external to Solidity. The integer type used is just
+    // large enough to hold all enum values, i.e. if you have more than 256 values,
+    // `uint16` will be used and so on.
+    function getChoice() public view returns (ActionChoices) {
+        return choice;
+    }
+
+    function getDefaultChoice() public pure returns (uint) {
+        return uint(defaultChoice);
+    }
 }
