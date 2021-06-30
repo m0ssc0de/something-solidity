491e13341d795a8c06363aa21f61996081d6e3f8
m0ssc0de <hi.paul.q@gmail.com>
2021-06-30 10:07:15 +0800

    update

---
+++ src/t004.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.7.0;
+
+contract SimpleAuction {
+    function hello() public pure returns (string memory) {
+        return "hello world";
+    }
+}
