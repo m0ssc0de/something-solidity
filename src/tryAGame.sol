bf9fef7ed91056178d10960a618fc104f0d3233d
m0ssc0de <hi.paul.q@gmail.com>
2021-06-30 18:23:38 +0800

    check action one by one. there is a big bug.

--- src/tryAGame.sol
+++ src/tryAGame.sol
@@ -2,18 +2,46 @@
 pragma solidity >=0.7.0  <0.9.0;
 
 contract RockPaperScissors {
-    enum ActionChoices { Rock, Paper, Scissors }
-    mapping(address => ActionChoices) actions;
-    function showRock() public {
-        actions[msg.sender] = ActionChoices.Rock;
+    enum ActionChoices { None, Rock, Paper, Scissors }
+    address public currentWinner;
+    ActionChoices public currentAction;
+    address[] users;
+    
+    mapping(address => ActionChoices) reacted;
+    
+    function myChoice(uint256 choice) public {
+        require(choice != 0, "please chooce a action from 1(Rock), 2(Paper), 3(Scissors)");
+        if (reacted[msg.sender] == ActionChoices.None) {
+            users.push(msg.sender);
+        }
+        reacted[msg.sender] = ActionChoices(choice);
     }
-    function showPaper() public {
-        actions[msg.sender] = ActionChoices.Paper;
+
+    function showMyChoice() public view returns (ActionChoices) {
+        return reacted[msg.sender];
     }
-    function showScissors() public {
-        actions[msg.sender] = ActionChoices.Scissors;
-    }
-    function myChoices() public view returns (ActionChoices) {
-        return actions[msg.sender];
+    
+    function findWinner() public payable  {
+        uint  length = users.length;
+        if (length == 0) {
+            return;
+        }
+        if (length == 1) {
+            currentWinner = users[0];
+            currentAction = reacted[currentWinner];
+            return;
+        }
+        
+        currentWinner = users[0];
+        currentAction = reacted[currentWinner];
+        
+        for (uint i = 1; i < (length); i++) {
+            int256 diff = int(reacted[users[i]]) - int(currentAction);
+            if (diff == 1) {
+                currentWinner = users[i];
+                currentAction = reacted[currentWinner];
+            }
+        }
+        // big bug
     }
 }
