5e8fa0716fd71da7de473f155b3473b0d24b2bf7
m0ssc0de <hi.paul.q@gmail.com>
2021-06-30 17:17:23 +0800

    blindAuction

--- src/t004.sol
+++ src/t004.sol
@@ -1,8 +1,49 @@
 // SPDX-License-Identifier: GPL-3.0
+
 pragma solidity ^0.7.0;
 
 contract SimpleAuction {
-    function hello() public pure returns (string memory) {
-        return "hello world";
+    address payable public beneficiary;
+    uint public auctionEnd;
+    address public highestBidder;
+    uint public highestBid;
+    mapping(address => uint) pendingReturns;
+    bool ended;
+    
+    constructor(uint _biddingTime, address payable _beneficiary) {
+        beneficiary = _beneficiary;
+        auctionEnd = block.timestamp + _biddingTime;
+    }
+    
+    function bid() public payable {
+        require(block.timestamp <= auctionEnd, "Auction already ended.");
+        require(msg.value > highestBid, "There already is a higher bid.");
+        if (highestBid != 0) {
+            pendingReturns[highestBidder] += highestBid;
+        }
+        highestBidder = msg.sender;
+        highestBid = msg.value;
+    }
+    
+    function withdraw() public returns (bool) {
+        uint amount = pendingReturns[msg.sender];
+        if (amount > 0) {
+            pendingReturns[msg.sender] = 0;
+            
+            if (!payable(msg.sender).send(amount)) {
+                pendingReturns[msg.sender] = amount;
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    function auctionEndd() public {
+        require(block.timestamp >= auctionEnd, "Auction not yet ended.");
+        require(!ended, "auctionEnd has already been called.");
+        
+        ended = true;
+        
+        beneficiary.transfer(highestBid);
     }
 }
